b33ee97609f4b374bf5bbe7e60bc3c4c7e96daea
ardnep <aranepal@gmail.com>
2022-11-02 22:24:40 +0800

    Added all classes

---
+++ MOBLIMA/src/HolidayManager.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class HolidayManager {
+    private static ArrayList<Holiday> holidays = new ArrayList<Holiday>();
+
+    public void addHoliday(Holiday holiday) {
+        holidays.add(holiday);
+    }
+
+    public boolean removeHoliday(Holiday holiday) {
+        return holidays.remove(holiday);
+    }
+
+    public static boolean isHoliday(int date) {
+        for(int i = 0; i < holidays.size(); i++) {
+            if(holidays.get(i).getDate() == date) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public ArrayList<Holiday> getHolidays() {
+        return holidays;
+    }
+
+    public void setHolidays(ArrayList<Holiday> holidays) {
+        HolidayManager.holidays = holidays;
+    }
+    
+}
