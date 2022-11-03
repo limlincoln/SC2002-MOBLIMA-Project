a993168b034f2a72d0379f524b25c6e11fccabc5
Lebarnon <53988015+Lebarnon@users.noreply.github.com>
2022-11-03 22:44:05 +0800

    Merged Kane Branch

--- MOBLIMA/src/entities/Cinema.java
+++ MOBLIMA/src/entities/Cinema.java
@@ -2,9 +2,41 @@ package entities;
 
 import java.util.ArrayList;
 
-public class Cinema {
+public class Cinema{
+    private int CinemaID;
+    private int CinemaType;
+    private int[] showTime;
+    
+    public Cinema(int cID, int cType, int[] sTime){
+        this.CinemaID = cID;
+        this.CinemaType = cType;
+        this.showTime = sTime;
+    }
+
+    // TODO: IMPLEMENT
     public ArrayList<Movie> getMovies(){
-        ArrayList<Movie> movies = new ArrayList<Movie>();
-        return movies;
+        return new ArrayList<Movie>();
+    }
+
+    //get methods
+    public int getCinemaType(){
+        return this.CinemaType;
+    }
+    public int getCinemaID(){
+        return this.CinemaID;
+    }
+    public int[] getShowTime(){
+        return this.showTime;
+    }
+    
+    //set methods
+    public void setCinemaType(int cType){
+        this.CinemaType = cType;
+    }
+    public void setCinemaID(int cID){
+        this.CinemaID = cID;
+    }
+    public void setShowTime(int[] sTime){
+        this.showTime = sTime;
     }
 }
--- MOBLIMA/src/entities/Cineplex.java
+++ MOBLIMA/src/entities/Cineplex.java
@@ -1,10 +1,46 @@
 package entities;
-
+//import java.util.ArrayArrayList;
 import java.util.ArrayList;
 
 public class Cineplex {
-    public ArrayList<Cinema> getCinemas(){
-        ArrayList<Cinema> cinemas = new ArrayList<Cinema>();
-        return cinemas;
+    private int CineplexID;
+    private String CineplexName;
+    private Cinema[] CinemaType;
+    private ArrayList<Cinema> cinemas;
+	private ArrayList<String> cinemaIDs;
+
+    public Cineplex(int cID, String cName, Cinema[] cType){
+        this.CineplexID = cID;
+        this.CineplexName = cName;
+        this.CinemaType = cType;
+    }
+
+    //get
+    public int getCineplexID(){
+        return this.CineplexID;
+    }
+    public String getCineplexName(){
+        return this.CineplexName;
+    }
+    public Cinema[] getCinemaType(){
+        return this.CinemaType;
+    }
+    public ArrayList<Cinema> getCinemas() {
+        return this.cinemas;
+    }
+	public ArrayList<String> getCinemaIDs() {
+        return this.cinemaIDs;
+    }
+
+    //set
+    public void setCineplexID(int cID){
+        this.CineplexID = cID;
+    }
+    public void setCineplexName(String cName){
+        this.CineplexName = cName;
+    }
+    public void setCinemaType(Cinema[] cType){
+        this.CinemaType = cType;
     }
 }
+
