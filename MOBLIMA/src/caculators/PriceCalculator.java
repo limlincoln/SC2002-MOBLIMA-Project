93bfe532a8460a6ebdfe9ebb62704507d6d75689
ardnep <aranepal@gmail.com>
2022-11-05 18:14:25 +0800

    created PriceCalculator

---
+++ MOBLIMA/src/caculators/PriceCalculator.java
@@ -0,0 +1,26 @@
+package caculators;
+
+import java.util.ArrayList;
+
+public class PriceCalculator {
+    public static double getGeneralPrice(ArrayList<IPrice> objs) {
+        double totalPrice = 0.0;
+
+        for(IPrice o: objs) {
+            totalPrice += o.getPrice();
+        }
+        return totalPrice;
+    }
+
+    public static double getTotalPrice(ArrayList<IPrice> individual, ArrayList<IPrice> general) {
+        double totalPrice = 0.0;
+        double generalPrice = getGeneralPrice(general);
+
+        for(IPrice o: individual) {
+            totalPrice += o.getPrice();
+            totalPrice += generalPrice;
+        }
+
+        return totalPrice;
+    }
+}
