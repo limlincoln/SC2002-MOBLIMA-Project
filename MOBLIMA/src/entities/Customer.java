c000dc83e9058662ac9bec719250413a21ed941e
ardnep <aranepal@gmail.com>
2022-11-05 18:54:37 +0800

    added getters and setters to Customer

--- MOBLIMA/src/entities/Customer.java
+++ MOBLIMA/src/entities/Customer.java
@@ -1,33 +1,49 @@
 package entities;
 
-import java.util.ArrayList;
-import java.util.List;
-
-
 public class Customer{
 
-	private Integer CustomerID;
-	
+	private Integer customerID;
+	private String userName;
+	private String email;
+    private Integer phoneNumber;
 
-	private String Name;
-	
-
-	private String Email;
-
-    private Integer PhoneNumber;
-
-
-    private List<String> Booking = new ArrayList<String>();
-
-
-
-	public Customer(int CustomerID, String Name, String Email, int PhoneNumber){
-		this.CustomerID = CustomerID;
-		this.Name= Name;
-    	this.Email= Email;
-    	this.PhoneNumber= PhoneNumber;
-    	
+	public Customer(int customerID, String userName, String email, int phoneNumber){
+		this.customerID = customerID;
+		this.userName= userName;
+    	this.email= email;
+    	this.phoneNumber= phoneNumber;
     }
 
+	// GETTERS & SETTERS
+	public Integer getCustomerID() {
+		return customerID;
+	}
 
+	public void setCustomerID(Integer customerID) {
+		this.customerID = customerID;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	} 
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Integer getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(Integer phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
 }
