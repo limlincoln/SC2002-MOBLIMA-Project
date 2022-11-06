c096f52c8435190817590c55db9db88b939dbb59
Lenson <53988015+Lebarnon@users.noreply.github.com>
2022-11-06 15:59:16 +0800

    Add top5 functions

--- MOBLIMA/src/consolidators/MovieConsolidator.java
+++ MOBLIMA/src/consolidators/MovieConsolidator.java
@@ -1,13 +1,15 @@
 package consolidators;
 
-import entities.Cineplex;
+import enums.Status;
+import managers.MovieManager;
 import entities.Movie;
-import entities.Cinema;
 import java.util.ArrayList;
+
+
 /*
  * Consolidates all movies in a given cineplex
  */
-public class MovieConsolidator implements IConsolidator<Movie, Cineplex> {
+public class MovieConsolidator implements IConsolidator<Movie> {
     private MovieConsolidator single_instance = null;
 
     private MovieConsolidator(){}
@@ -19,15 +21,47 @@ public class MovieConsolidator implements IConsolidator<Movie, Cineplex> {
         return single_instance;
     }
     
-    public ArrayList<Movie> getAll(Cineplex cineplex){
-        ArrayList<Cinema> cinemas = cineplex.getCinemas();
-        ArrayList<Movie> movies = new ArrayList<Movie>();
-        for(int i=0; i<cinemas.size(); i++){
-            movies.addAll(cinemas.get(i).getMovies());
-        }
-        return movies;
+    public ArrayList<Movie> getAll(){
+        MovieManager movieManager = MovieManager.getInstance();
+        return movieManager.getMovies();
     }
     public ArrayList<Movie> getShowing(){
-        return new ArrayList<Movie>();
+        Status showing = Status.Showing;// settings.getShowingValue()
+        ArrayList<Movie> result = this.getAll();
+        for(int i=0; i<result.size(); i++){
+            if(result.get(i).getStatus() != showing){
+                result.remove(i);
+            }
+        }
+        return result;
+    }
+    public ArrayList<Movie> getTop5ByRating(){
+        ArrayList<Movie> result = this.getAll();
+        result.sort((m1, m2) -> {
+            if(m1.getAvgRating() > m2.getAvgRating())
+                return -1;
+            if(m1.getAvgRating() < m2.getAvgRating())
+                return 1;
+            return 0;
+        });
+        for(int i=5; i<result.size(); i++){
+            result.remove(i);
+        }
+        return result;
+    }
+
+    public ArrayList<Movie> getTop5BySales(){
+        ArrayList<Movie> result = this.getAll();
+        result.sort((m1, m2) -> {
+            if(m1.getTotalSales() > m2.getTotalSales())
+                return -1;
+            if(m1.getTotalSales() < m2.getTotalSales())
+                return 1;
+            return 0;
+        });
+        for(int i=5; i<result.size(); i++){
+            result.remove(i);
+        }
+        return result;
     }
 }
