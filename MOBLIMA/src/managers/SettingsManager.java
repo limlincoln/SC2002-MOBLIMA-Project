84b7a307f0a3c963b7031f2674b3c97f23f942e7
Lenson <53988015+Lebarnon@users.noreply.github.com>
2022-11-11 01:23:44 +0800

    add SettingsManager

--- MOBLIMA/src/managers/SettingsManager.java
+++ MOBLIMA/src/managers/SettingsManager.java
@@ -1,3 +1,41 @@
+package managers;
+
+public class SettingsManager {
+	private static SettingsManager single_instance = null;
+	private int customerTop5;
+	private SettingsManager(int customerTop5){
+		this.customerTop5 = customerTop5;
+	}
+	public static SettingsManager getInstance(){
+		return single_instance;
+	}
+
+	/**
+	 * Initialises a single instance of settings manager
+	 * @param customerTop5Value
+	 */
+	public static void initialiser(int customerTop5Value){
+		single_instance = new SettingsManager(customerTop5Value);
+	}
+
+	public void editPrices(){
+		
+	}
+
+
+	/**
+	 * Getter and Setters
+	 */
+	public int getCustomerTop5() {
+		return this.customerTop5;
+	}
+
+	public void setCustomerTop5(int customerTop5) {
+		this.customerTop5 = customerTop5;
+	}
+
+}	
+
 //TODO:
 	// ATTRIBUTES:
 	// WHICH TOP 5
