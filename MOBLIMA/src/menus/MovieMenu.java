926f894e7d7cdc190b0fdd31f3eb25df1bdd5341
kanetan4 <kanetan4@gmail.com>
2022-11-11 14:29:27 +0800

    Update MovieMenu.java

--- MOBLIMA/src/menus/MovieMenu.java
+++ MOBLIMA/src/menus/MovieMenu.java
@@ -91,8 +91,6 @@ public class MovieMenu {
 	
 	public void displayStaffMovieMenu() {
 		int choice;
-
-		
 		do {
             System.out.println(	"============== MOBLIMA STAFF APP ================\n" +
 			                    " 1. Edit Movie          					        \n" +
