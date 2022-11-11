b40d9e6581f87241a03d557e83cd8d357561a77a
kanetan4 <kanetan4@gmail.com>
2022-11-11 14:39:09 +0800

    Update SettingsManager.java

--- MOBLIMA/src/managers/SettingsManager.java
+++ MOBLIMA/src/managers/SettingsManager.java
@@ -93,88 +93,14 @@ public class SettingsManager {
 	// WHICH TOP 5
 
 	// FUNCTION:
-	// EDIT PRICES
-	// EDIT MOVIES
+	// EDIT PRICES - DONE
+	// EDIT MOVIES - DONE
 	// EDIT HOLIDAYS - DONE
 
-
+	
 
 	// PRICE FUNCTIONS
 	// 
-
-
-	
-	/* 
-	// MOVIE FUNCTIONS
-	// display movie  - DONE
-	// search movie - 
-	// add movie - 
-	// remove movie - DONE
-	public void displayMovies(){
-		private ArrayList<Movie> movies = new ArrayList<Movie>();
-		movies = MovieManager.getMovies();
-		for (int y = 0; y < holiday.size(); y++){
-			System.out.println(y+1 + ". " + movies.get(y).getMovieName());
-		}
-	}
-
-	public void searchMovies(){
-		Scanner sc = new Scanner(System.in);
-		private ArrayList<Movie> moviess = new ArrayList<Movie>();
-		moviess = MovieManager.getMovies();
-		String name;
-		System.out.println("Enter Movie Name to search: ");
-		name = sc.next();
-
-		for (int z = 0; x < moviess.size(); z++){
-			if (moviess.get(z).getMovieName() == name){
-				//print all the values of the movies
-			}
-		}
-	}
-
-	public void addNewMovie(){
-		Scanner sc = new Scanner(System.in);
-
-	}
-
-	public void removeExistingMovie(){
-		Scanner sc = new Scanner(System.in);
-		private ArrayList<Movie> moviesss = new ArrayList<Movie>();
-		moviesss = MovieManager.getMovies();
-		
-		Scanner sc = new Scanner(System.in);
-		int choice;
-		do {
-			System.out.println("=================== Remove Movie ===================");
-			System.out.println("1. Remove by Name");
-			System.out.println("2. Remove by ID");
-			System.out.println("0. Exit");
-			switch(choice){
-				case 1:
-					String name;
-					System.out.println("Please enter the Name: ");
-					name = sc.next();
-					MovieManager.getInstance().removeMovieByName(name);
-					break;
-				case 2:
-					int id;
-					System.out.println("Please enter the ID: ");
-					id = sc.nextInt();
-					MovieManager.getInstance().removeMovieByID(id);
-					break;
-				case 0:
-					System.out.println("Back to Settings Menu......");
-					break;
-				default:
-					System.out.println("Invalid choice. Please choose between 0-8.");
-					break;
-			}
-		} while (choice != 0);
-	}*/
-
-
-
 
 	// HOLIDAY FUNCTIONS - DONE
 	// list holidays - DONE
