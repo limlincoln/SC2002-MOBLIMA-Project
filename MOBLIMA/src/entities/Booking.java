8c2afb8de5c038dca618e3adc43686d0ce4d058c
ardnep <aranepal@gmail.com>
2022-11-11 19:33:08 +0800

    Added javadocs to Booking

--- MOBLIMA/src/entities/Booking.java
+++ MOBLIMA/src/entities/Booking.java
@@ -5,13 +5,45 @@ import java.util.ArrayList;
 import managers.MovieManager;
 
 public class Booking {
+    /**
+     * The transaction ID in the format XXXYYYYMMDDhh
+     * (XXX = cinema code)
+     */
     private int TID;
+
+    /**
+     * ID of movie for which the Booking was made
+     */
     private int movieID;
+
+    /**
+     * The Customer who booked
+     */
     private Customer customer;
+
+    /**
+     * The list of Tickets booked
+     */
     private ArrayList<Ticket> tickets;
+
+    /**
+     * The rating given to the movie in this booking
+     */
     private int rating = -1;
+
+    /**
+     * The total cost for this Booking
+     */
     private double totalCost;
 
+    /**
+     * Constructor for the Booking Class
+     * @param tid The transaction ID of this Booking
+     * @param movieID The ID of the movie for which this Booking was made
+     * @param customer The Customer who booked this Booking
+     * @param tickets The List of Tickets booked for this Booking
+     * @param totalCost The total cost for this Booking
+     */
     public Booking(int tid, int movieID, Customer customer, ArrayList<Ticket> tickets, double totalCost) {
         this.TID = tid;
         this.movieID = movieID;
@@ -20,6 +52,9 @@ public class Booking {
         this.totalCost = totalCost;
     }
 
+    /**
+     * Prints out the details of the Booking on screen
+     */
     public void showBookingDetails() {
         System.out.println(
             "TID: "+ TID + "\n" +
