d2990913902cde23a83d92f222e1dd4ca93f9c45
ardnep <aranepal@gmail.com>
2022-11-11 19:36:39 +0800

    Added javadocs to Cinema

--- MOBLIMA/src/entities/Cinema.java
+++ MOBLIMA/src/entities/Cinema.java
@@ -8,11 +8,27 @@ import managers.PricingManager;
 import managers.ShowTimeManager;
 
 public class Cinema implements IPrice {
-    private String CinemaName;
+    /**
+     * The ID of the Cinema
+     */
     private int CinemaID;
+
+    /**
+     * The type of this Cinema
+     */
     private CinemaType CinemaType;
+
+    /**
+     * The list of ShowTimes for this Cinema
+     */
     private ArrayList<ShowTime> showtimes;
     
+    /**
+     * Constructor for the Cinema Class
+     * @param cID The ID of this Cinema
+     * @param cType The type of this Cinema
+     * @param sTime The list of ShowTimes for this Cinema
+     */
     public Cinema(int cID, CinemaType cType, ArrayList<ShowTime> sTime){
         this.CinemaID = cID;
         this.CinemaType = cType;
@@ -21,11 +37,6 @@ public class Cinema implements IPrice {
 
     public double getPrice() {
         return PricingManager.getPrice(CinemaType);
-    }
-
-    // TODO: IMPLEMENT
-    public ArrayList<Movie> getMovies(){
-        return new ArrayList<Movie>();
     }
 
     public boolean containsMovie(int movieID) {
