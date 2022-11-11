6c4a7776ba89fb320fc1f61f6d2d1bbe7569b490
ardnep <aranepal@gmail.com>
2022-11-11 21:15:00 +0800

    Added javadocs to Space

--- MOBLIMA/src/entities/Space.java
+++ MOBLIMA/src/entities/Space.java
@@ -1,7 +1,15 @@
 package entities;
 
 public class Space implements ISeat{
+    /**
+     * The Constructor for the Space Class
+     */
     public Space(){}; 
+
+    /**
+     * get the state of this space
+     * @return always a " "
+     */
     @Override
     public String getState(){
         return " ";
