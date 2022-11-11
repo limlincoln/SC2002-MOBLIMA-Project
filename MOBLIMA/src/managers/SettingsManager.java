8e51da1d574a7408567d1a1855b37d73bf97046f
kanetan4 <kanetan4@gmail.com>
2022-11-11 14:29:24 +0800

    Update SettingsManager.java

--- MOBLIMA/src/managers/SettingsManager.java
+++ MOBLIMA/src/managers/SettingsManager.java
@@ -103,6 +103,8 @@ public class SettingsManager {
 	// 
 
 
+	
+	/* 
 	// MOVIE FUNCTIONS
 	// display movie  - DONE
 	// search movie - 
@@ -169,7 +171,7 @@ public class SettingsManager {
 					break;
 			}
 		} while (choice != 0);
-	}
+	}*/
 
 
 
