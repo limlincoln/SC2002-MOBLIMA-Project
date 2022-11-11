8dccf0e5058f679bc105fb8fc1080ecc21d7e98e
Lenson <53988015+Lebarnon@users.noreply.github.com>
2022-11-11 15:20:01 +0800

    Update ShowTime Manager

--- MOBLIMA/src/managers/ShowtimeManager.java
+++ MOBLIMA/src/managers/ShowtimeManager.java
@@ -1,25 +1,30 @@
 package managers;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
+import entities.Cinema;
+import entities.Movie;
+import entities.Seats;
 //import entities.Movie;
 //import managers.MovieManager;
 import entities.ShowTime;
+import enums.DayOfWeek;
 
 //need to filter by movie or cinplex??
 
 
 public class ShowTimeManager{
     private static ShowTimeManager single_instance = null;
-    //private ArrayList<Movie> movies;
-    private ArrayList<ShowTime> ShowTimes;
-    private Scanner sc = new Scanner(System.in);
-// FUNCTIONALITY
-// SHOW ShowTime FOR A PARTICULAR MOVIE & DAY & CINEPLEX & CINEMA
-// RETURN ARRAY OF ShowTime
 
-// FINDSEATS:
-// FIND THE SEATS FROM CINEMA CLASS USING ShowTime SLOT INDEX
+    private Scanner sc = new Scanner(System.in);
+    // FUNCTIONALITY
+    // SHOW ShowTime FOR A PARTICULAR MOVIE & DAY & CINEPLEX & CINEMA
+    // RETURN ARRAY OF ShowTime
+
+    // FINDSEATS:
+    // FIND THE SEATS FROM CINEMA CLASS USING ShowTime SLOT INDEX
 
     // Initialisers
     private ShowTimeManager(){}
@@ -30,36 +35,35 @@ public class ShowTimeManager{
         return single_instance;
     }
 
-    // Getters
-    public ArrayList<ShowTime> getShowTimes(){
-        return this.ShowTimes;
-    }
-    public ShowTime getShowTimeByID(int sID){
-        for(int x = 0; x < ShowTimes.size(); x++){
-            if(ShowTimes.get(x).getShowTimeID() == sID){
-                return ShowTimes.get(x);
+    /**
+     * Get all date and time where a particular movie in a cinema is showing
+     * @param cinema
+     * @param movie
+     * @return An arraylist of localdate time where the movie is showing
+     */
+
+    public ArrayList<LocalDateTime> getShowTimeByMovie(Cinema cinema, Movie movie){
+        ArrayList<LocalDateTime> showDateTimes = new ArrayList<LocalDateTime>();
+        int movieID = movie.getMovieID();
+        ArrayList<ShowTime> showtimes = cinema.getShowtimes();
+        int numOfTimeSlots = showtimes.get(0).getShowTime().length;
+
+        for(int i=0; i<showtimes.size(); i++){
+            int[] curShowTimeSlots = showtimes.get(i).getShowTime();
+            for(int j=0; j<numOfTimeSlots; j++){
+                if(curShowTimeSlots[j] == movieID){
+                    showDateTimes.add(DateManager.getInstance().getExactShowTime(DayOfWeek.values()[i],j));
+                }
             }
         }
-        return null;
+        return showDateTimes;
     }
     
-    // Setters
-    public void setShowTime(ArrayList<ShowTime> stime){
-        this.ShowTimes = stime;
+    public Seats getSeatsByLocalDateTime(Cinema cinema, LocalDateTime localDateTime){
+        int dayOfWeekInInt = localDateTime.getDayOfWeek().getValue();
+        int selectedTimeSlot = DateManager.getInstance().getTimeSlotIndex(localDateTime.toLocalTime().of(23, 0));
+        return cinema.getShowtimes().get(dayOfWeekInInt).getSeats()[selectedTimeSlot];
+
     }
 
-    // Methods
-    // Add ShowTimes
-    public void addShowTime(ShowTime stime){
-        for(int x = 0; x < ShowTimes.size(); x++){
-            ShowTime current = ShowTimes.get(x);
-            if(current.getShowTimeID() == stime.getShowTimeID() 
-               || current.getShowTimeID()==stime.getShowTimeID()){
-                System.out.println("ShowTime already exist!");
-                return;
-            }
-        }
-        ShowTimes.add(stime);
-        System.out.println("ShowTime successfully added!");
-    }
 }
