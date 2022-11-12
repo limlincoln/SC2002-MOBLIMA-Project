11e2b329f12e360f3d2475bcb1aa9a8a47868772
limlincoln <limlincoln98@gmail.com>
2022-11-12 12:32:08 +0800

    Javadocs for calculators

--- MOBLIMA/src/caculators/PriceCalculator.java
+++ MOBLIMA/src/caculators/PriceCalculator.java
@@ -6,7 +6,19 @@ import entities.IPrice;
 import managers.PricingManager;
 import enums.PriceType;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class PriceCalculator.
+ */
 public class PriceCalculator {
+    
+    /**
+	 * Gets the total price.
+	 *
+	 * @param objs        - the objs
+	 * @param noOfTickets - number of tickets
+	 * @return the total price
+	 */
     public static double getTotalPrice(ArrayList<IPrice> objs, int noOfTickets) {
         double totalPrice = 0.0;
         double basePrice = PricingManager.getPrice(PriceType.BASE_PRICE);
--- MOBLIMA/src/caculators/SalesCalculator.java
+++ MOBLIMA/src/caculators/SalesCalculator.java
@@ -4,11 +4,25 @@ import java.util.ArrayList;
 
 import entities.Movie;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class SalesCalculator.
+ */
 public class SalesCalculator {
+    
+    /** The single instance. */
     private static SalesCalculator single_instance = null;
 
+    /**
+	 * Instantiates a new sales calculator.
+	 */
     private SalesCalculator(){}
 
+    /**
+	 * Gets the single instance of SalesCalculator.
+	 *
+	 * @return single instance of SalesCalculator
+	 */
     public static SalesCalculator getInstance(){
         if(single_instance == null){
             single_instance = new SalesCalculator();
@@ -16,6 +30,12 @@ public class SalesCalculator {
         return single_instance;
     }
     
+    /**
+	 * Calc.
+	 *
+	 * @param movies the movies
+	 * @return the double
+	 */
     public double calc(ArrayList<Movie> movies){
         double total = 0;
         for(int i=0; i<movies.size(); i++){
