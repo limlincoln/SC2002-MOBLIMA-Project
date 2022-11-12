819ae21716fc2d55a96d266f0877c7e6f42ca929
ardnep <aranepal@gmail.com>
2022-11-12 12:12:06 +0800

    SeatInitializere

--- MOBLIMA/src/initializer/SeatsInitializer.java
+++ MOBLIMA/src/initializer/SeatsInitializer.java
@@ -5,7 +5,6 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 
 import entities.ISeat;
 import entities.Seat;
