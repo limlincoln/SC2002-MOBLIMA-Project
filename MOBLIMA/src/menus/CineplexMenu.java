2eb10cfb5c67c92cf65ed15474197c4427dc38a8
ardnep <aranepal@gmail.com>
2022-11-13 02:21:29 +0800

    i think its done

--- MOBLIMA/src/menus/CineplexMenu.java
+++ MOBLIMA/src/menus/CineplexMenu.java
@@ -28,8 +28,6 @@ public class CineplexMenu {
      */
 	public void displayCineplexMenu(Movie movie) { 
         ArrayList<Cineplex> cineplexes = CineplexManager.getInstance().getCineplexs();
-        // System.out.println(CineplexManager.getInstance().getCineplexs().get(0).getCinemas().get(0).containsMovie(movie.getMovieID()));
-        // System.out.println("Cineplexes: "+cineplexes);
         Cineplex selectedCinplex = CineplexSelectorMenu.getInstance().startSelector(cineplexes, "SELECT A CINEPLEX");
 
         if (selectedCinplex == null) return;
